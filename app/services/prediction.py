137017884b08820c450a62922d6a76687c6b07d8
N.Bharath <81911216+nbharath1306@users.noreply.github.com>
2025-04-11 16:57:44 +0530

    Create prediction.py

---
+++ app/services/prediction.py
@@ -0,0 +1,9 @@
+# Dummy model logic
+
+def predict_resources(data: dict):
+    return {
+        "predicted_admissions": 34,
+        "average_los": 5.2,
+        "bed_needs": 12,
+        "equipment_demand": 7
+    }
